6378039d2202584ba3c0c3c268834f85714497cf
Rohit Shrivastava <rohitxsh@gmail.com>
2022-06-26 20:15:55 +0530

    chore: update readme, gitignore, remove local location

--- README.md
+++ README.md
@@ -6,17 +6,17 @@ Dependency - `config.toml`
 `config.toml`
 ```
 [[config]]
-species = "string, DB name / local file path (if location == 'local')"
-location = "string, DB server address / 'local'"
+species = "string, DB name"
+location = "string, DB server address"
 port = "string, DB server's port no."
-DB_USER = "string, username to access the specified DB server, required if location is a DB server address"
+DB_USER = "string, username to access the specified DB server"
 [[config.data.tables]]
 table = "string, table name"
 query = '''
-milti-line string, SQL query to construct the table, variables can we used that are defined in vars array
+milti-line string, SQL query to construct the table, variables can we used that are defined in vars
 '''
-columns = "string with column names separated by commas, needed if columns names need to be renamed in the new table"
-vars = ["string"], params to be used in the query
+[config.data.tables.vars]
+var_key = "string, var_value"
 ```
 
 Supported DB: MySQL
--- code.py
+++ code.py
@@ -11,21 +11,14 @@ config = toml.load(open('config.toml'))["config"]
 if not os.path.exists('parquet'): os.mkdir('parquet')
 
 for elem in config:
-    if (elem["location"] == "local"):
-        pass
-    else:
-        db_connection_str = f'mysql+pymysql://{elem["DB_USER"]}@{elem["location"]}:{elem["port"]}/{elem["species"]}'
-        engine = create_engine(db_connection_str)
-
-        for table in elem["data"]["tables"]:
-            query = table["query"]
-            print(table["vars"])
-            df = pd.read_sql(query, con=engine, params=table["vars"])
-
-            if not os.path.exists(f'parquet{os.sep}{elem["species"]}'):
-                os.mkdir(f'parquet{os.sep}{elem["species"]}')
-
-            # engine: pyarrow
-            df.to_parquet(f'parquet{os.sep}{elem["species"]}{os.sep}{table["table"]}.parquet')
+    db_connection_str = f'mysql+pymysql://{elem["DB_USER"]}@{elem["location"]}:{elem["port"]}/{elem["species"]}'
+    engine = create_engine(db_connection_str)
+    for table in elem["data"]["tables"]:
+        query = table["query"]
+        df = pd.read_sql(query, con=engine, params=table["vars"])
+        if not os.path.exists(f'parquet{os.sep}{elem["species"]}'):
+            os.mkdir(f'parquet{os.sep}{elem["species"]}')
+        # engine: pyarrow
+        df.to_parquet(f'parquet{os.sep}{elem["species"]}{os.sep}{table["table"]}.parquet')
 
 print(f'It took {time()-start} secs.')
